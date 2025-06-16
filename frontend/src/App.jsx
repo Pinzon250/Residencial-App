3edebe7ebdd7b2677be910a0eeaf70d9d5f9b28c
Pinzon250 <santiagopinzo@gmail.com>
2025-06-16 13:21:06 -0500

    feat: Refactor routing structure and improve admin module organization in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,16 +5,28 @@ import "./App.css";
 import Layout from "./layout/Layout";
 
 // Autenticacion
-import ProtectedRoute from "./components/auth/ProtectedRoute";
-import CambiarContraseña from "./pages/ChangePassword";
-import Login from "./pages/Login";
-import PublicRoute from "./components/auth/PublicRoute";
+import ProtectedRoute from "./hooks/auth/ProtectedRoute";
+import CambiarContraseña from "./pages/auth/ChangePassword";
+import Login from "./pages/auth/Login";
+import PublicRoute from "./hooks/auth/PublicRoute";
 
 // Paginas administrador
 import Usuarios from "./pages/admin/Usuarios";
+import Calendario from "./pages/admin/Calendario";
+import Archivos from "./pages/admin/Archivos";
+import Balance from "./pages/admin/Balance";
+import Registros from "./pages/admin/Registros";
 
 // Paginas
 import Home from "./pages/Home";
+
+// Modulos principales
+import Pagos from "./pages/modules/Pagos";
+import Documentos from "./pages/modules/Documentos";
+import Historial from "./pages/modules/Historial";
+import Novedades from "./pages/modules/Novedades";
+import Reservas from "./pages/modules/Reservas";
+
 
 
 
@@ -22,10 +34,93 @@ import Home from "./pages/Home";
 function App() {
   return (
       <Routes>
-        <Route path="/" element={<PublicRoute><Login /></PublicRoute>}/>
-        <Route path="/home" element={<ProtectedRoute><Layout><Home /></Layout></ProtectedRoute>} />
-        <Route path="/cambiar-contraseña" element={<ProtectedRoute><CambiarContraseña /></ProtectedRoute>}/>
-        <Route path="/usuarios" element={<ProtectedRoute><Layout><Usuarios /></Layout></ProtectedRoute>}/>
+
+        {/* Login */}
+        <Route path="/" element={
+          <PublicRoute>
+            <Login />
+          </PublicRoute>}/>
+
+        {/* Cambiar contraseña */}
+        <Route path="/cambiar-contraseña" element={
+          <ProtectedRoute>
+            <CambiarContraseña />
+          </ProtectedRoute>}/>
+
+{/*---------------- Modulos Generales ------------- */}
+
+        {/* Home */}
+        <Route path="/home" element={
+          <ProtectedRoute>
+            <Layout><Home /></Layout>
+        </ProtectedRoute>} />
+
+        {/* Pagos */}
+        <Route path="/pagos" element={
+          <ProtectedRoute>
+            <Layout><Pagos /></Layout>
+        </ProtectedRoute>} />
+
+        {/* Documentos */}
+        <Route path="/documentos" element={
+          <ProtectedRoute>
+            <Layout><Documentos /></Layout>
+        </ProtectedRoute>} />
+
+        {/* Historial */}
+        <Route path="/historial" element={
+          <ProtectedRoute>
+            <Layout><Historial /></Layout>
+        </ProtectedRoute>} />
+
+        {/* Novedades */}
+        <Route path="/novedades" element={
+          <ProtectedRoute>
+            <Layout><Novedades /></Layout>
+        </ProtectedRoute>} />
+
+        {/* Reservas */}
+        <Route path="/reservas" element={
+          <ProtectedRoute>
+            <Layout><Reservas /></Layout>
+        </ProtectedRoute>} />
+
+
+{/*---------------- Modulos Administradores ------------- */}
+
+        {/* Usuarios : Administrador */}
+        <Route path="/usuarios" element={
+          <ProtectedRoute role="administrador">
+            <Layout><Usuarios /></Layout>
+        </ProtectedRoute>}/>
+
+        {/* Calendario : Administrador */}
+        <Route path="/calendario" element={
+          <ProtectedRoute role="administrador">
+            <Layout><Calendario /></Layout>
+        </ProtectedRoute>}/>
+
+        {/* Archivos : Administrador */}
+        <Route path="/archivos" element={
+          <ProtectedRoute role="administrador">
+            <Layout><Archivos /></Layout>
+        </ProtectedRoute>}/>
+
+        {/* Balance : Administrador */}
+        <Route path="/balance" element={
+          <ProtectedRoute role="administrador">
+            <Layout><Balance /></Layout>
+        </ProtectedRoute>}/>
+
+        {/* Registros : Administrador */}
+        <Route path="/registros" element={
+          <ProtectedRoute role="administrador">
+            <Layout><Registros /></Layout>
+        </ProtectedRoute>}/>
+
+
+
+
       </Routes>
   );
 } 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./hooks/AuthContext.jsx";
+import { AuthProvider } from "./hooks/auth/AuthContext.jsx";
 import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
